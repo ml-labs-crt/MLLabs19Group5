b866e2ca8d051563e8e98ad487c206572134e759
JamesFitzpatrickMLLabs <56206655+JamesFitzpatrickMLLabs@users.noreply.github.com>
2019-10-24 18:18:26 +0100

    Add files via upload

--- bikes/app_utils.py
+++ bikes/app_utils.py
@@ -7,12 +7,44 @@ fun = api_utils.get_data_WB
 
 
 @app.route("/<year>/<month>/<day>/<hour>/<minute>/<second>")
-def main(year, month, day, hour, minute, second):
+def weather(year, month, day, hour, minute, second):
     year, month, day = int(year), int(month), int(day)
     hour, minute, second = int(hour), int(minute), int(second)
     
     return fun(year, month, day, hour, minute, second)
 
 
-if __name__ == "_main__":
+@app.route("/<dep>/<year>/<month>/<day>/<hour>/<minute>/<second>/<arr>")
+def station(dep, arr, year, month, day, hour, minute, second):
+    request = {
+        "departure_station": dep,
+        "Year": int(year),
+        "Month": int(month),
+        "Day": int(day),
+        "Hour": int(hour),
+        "Minute": int(minute),
+        "Second": int(second),
+        "arrival_station": arr
+    }
+    
+    return api_utils.get_all_infos(request)
+
+    
+@app.route("/<dep>/<year>/<month>/<day>/<hour>/<minute>/<second>/<arr>/<predict>")
+def silly_predict(dep, arr, year, month, day, hour, minute, second, predict):
+    request = {
+        "departure_station": dep,
+        "Year": int(year),
+        "Month": int(month),
+        "Day": int(day),
+        "Hour": int(hour),
+        "Minute": int(minute),
+        "Second": int(second),
+        "arrival_station": arr
+    }
+    print(request)
+    return api_utils.get_silly_estimates(request)
+
+
+if __name__ == "__main__":
     app.run()
