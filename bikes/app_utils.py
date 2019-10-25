24c1bcb18e0f7aa3f485fa72928350acbd896ff6
JamesFitzpatrickMLLabs <56206655+JamesFitzpatrickMLLabs@users.noreply.github.com>
2019-10-25 11:42:54 +0100

    Add files via upload

--- bikes/app_utils.py
+++ bikes/app_utils.py
@@ -16,34 +16,62 @@ def weather(year, month, day, hour, minute, second):
 
 @app.route("/<dep>/<year>/<month>/<day>/<hour>/<minute>/<second>/<arr>")
 def station(dep, arr, year, month, day, hour, minute, second):
+    year, month, day = int(year), int(month), int(day)
+    hour, minute, second = int(hour), int(minute), int(second)
+
     request = {
         "departure_station": dep,
-        "Year": int(year),
-        "Month": int(month),
-        "Day": int(day),
-        "Hour": int(hour),
-        "Minute": int(minute),
-        "Second": int(second),
+        "Year": year,
+        "Month": month,
+        "Day": day,
+        "Hour": hour,
+        "Minute": minute,
+        "Second": second,
         "arrival_station": arr
     }
     
     return api_utils.get_all_infos(request)
 
     
-@app.route("/<dep>/<year>/<month>/<day>/<hour>/<minute>/<second>/<arr>/<predict>")
-def silly_predict(dep, arr, year, month, day, hour, minute, second, predict):
+@app.route("/predict/<dep>/<year>/<month>/<day>/<hour>/<minute>/<second>/<arr>/")
+def silly_predict(dep, arr, year, month, day, hour, minute, second):
+
     request = {
         "departure_station": dep,
-        "Year": int(year),
-        "Month": int(month),
-        "Day": int(day),
-        "Hour": int(hour),
-        "Minute": int(minute),
-        "Second": int(second),
+        "Year": year,
+        "Month": month,
+        "Day": day,
+        "Hour": hour,
+        "Minute": minute,
+        "Second": second,
         "arrival_station": arr
     }
-    print(request)
-    return api_utils.get_silly_estimates(request)
+
+    package = api_utils.get_all_infos(request)
+    return api_utils.predict_packages(package)
+
+
+@app.route("/data/<dep>/<year>/<month>/<day>/<hour>/<minute>/<second>/<arr>/")
+def get_all_data(dep, arr, year, month, day, hour, minute, second):
+
+    request = {
+        "departure_station": dep,
+        "Year": year,
+        "Month": month,
+        "Day": day,
+        "Hour": hour,
+        "Minute": minute,
+        "Second": second,
+        "arrival_station": arr
+    }
+
+    stations = api_utils.get_all_infos(request)
+    conditions = weather(year, month, day, hour, minute, second)
+    for key in stations.keys():
+        stations[key].update(conditions)
+
+    return stations
+    
 
 
 if __name__ == "__main__":
